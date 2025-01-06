a811d688890d7e44819f27f4470beca0e7c0252f
Kishore <ganapathinaicker@gmail.com>
2025-01-06 21:16:16 +0530

    Time: 5 ms (61.46%), Space: 44.8 MB (46.88%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,16 +1,35 @@
 class Solution {
     public int[] minOperations(String boxes) {
-        HashSet<Integer> set=new HashSet<>();
+        int rightSum=0;
+        int rightCount=0;
         for(int i=0;i<boxes.length();i++){
             if(boxes.charAt(i)=='1'){
-                set.add(i);
+                rightSum+=i;
+                rightCount++;
             }
         }
+        int leftSum=0;
+        int leftCount=0;
         int[] result=new int[boxes.length()];
-        for(int i=0;i<result.length;i++){
+        result[0]=rightSum;
+        if(boxes.charAt(0)=='1'){
+            rightCount--;
+            leftCount++;
+        }
+        for(int i=1;i<result.length;i++){
             int distance=0;
-            for(int index:set){
-                distance+=Math.abs(index-i);
+            if(boxes.charAt(i)=='1'){
+                rightSum-=i;
+                rightCount--;
+                distance+=Math.abs((i*rightCount)-rightSum);
+                leftSum+=i;
+                leftCount++;
+            }
+            else{
+                distance+=Math.abs((i*rightCount)-rightSum);
+            }
+            if(leftCount>0){
+                distance+=Math.abs((i*leftCount)-leftSum);
             }
             result[i]=distance;
         }
