5ea2e73c6113326976b33a99d225036d83c52b89
Kishore <ganapathinaicker@gmail.com>
2025-01-21 00:56:11 +0530

    Time: 42 ms (19.9%), Space: 71.4 MB (13.93%) - LeetHub

--- 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.java
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.java
@@ -1,25 +1,22 @@
 class Solution {
     public int firstCompleteIndex(int[] arr, int[][] mat) {
+        HashMap<Integer,int[]> indexMap=new HashMap<>();
         HashMap<Integer,Integer> rowMap=new HashMap<>();
         HashMap<Integer,Integer> colMap=new HashMap<>();
         int row=mat.length;
         int col=mat[0].length;
-        int[][] freq=new int[row*col+1][2];
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
-                freq[mat[i][j]]=new int[]{i,j};
+                indexMap.put(mat[i][j],new int[]{i,j});
             }
         }
         for(int i=0;i<arr.length;i++){
-            int[] index=freq[arr[i]];
+            int[] index=indexMap.get(arr[i]);
             rowMap.put(index[0],rowMap.getOrDefault(index[0],0)+1);
-            if(rowMap.get(index[0])>=col){
-                return i;
-            }
+            if(rowMap.get(index[0])>=col)return i;
             colMap.put(index[1],colMap.getOrDefault(index[1],0)+1);
-            if(colMap.get(index[1])>=row){
-                return i;
-            }
+            if(colMap.get(index[1])>=row)return i;
+            
         }
         return 0;
     }
