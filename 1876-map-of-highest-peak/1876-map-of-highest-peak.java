6b41d9f125d850161a477a2880893609f3649cbf
Kishore <ganapathinaicker@gmail.com>
2025-01-23 01:11:07 +0530

    Time: 48 ms (75.05%), Space: 167.5 MB (58.41%) - LeetHub

---
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[][] highestPeak(int[][] isWater) {
+        LinkedList<int[]> queue=new LinkedList<>();
+        int row=isWater.length;
+        int col=isWater[0].length;
+        int[][] result=new int[row][col];
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(isWater[i][j]==1){
+                    queue.offer(new int[]{i,j});
+                    result[i][j]=0;
+                }
+                else{
+                    result[i][j]=-1;
+                }
+            }
+        }
+        int[][] directions={{1,0},{0,1},{0,-1},{-1,0}};
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            for(int i=0;i<size;i++){
+                int[] index=queue.poll();
+                for(int[] dir:directions){
+                    int newRow=index[0]+dir[0];
+                    int newCol=index[1]+dir[1];
+                    if(newRow<0 || newRow>=row || newCol>=col || newCol<0 || result[newRow][newCol]!=-1)continue;
+                    result[newRow][newCol]=result[index[0]][index[1]]+1;
+                    queue.offer(new int[]{newRow,newCol});
+                }
+            }
+        }
+        return result;
+    }
+}
