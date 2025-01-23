630c922c0e7c1577bbe3997c7e6c6f3e6d7da841
Kishore <ganapathinaicker@gmail.com>
2025-01-24 00:52:25 +0530

    Time: 2 ms (100%), Space: 52.9 MB (12.99%) - LeetHub

--- 1396-count-servers-that-communicate/1396-count-servers-that-communicate.java
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.java
@@ -5,44 +5,34 @@ class Solution {
         int col=grid[0].length;
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
-                if(grid[i][j]==1){
-                    serverCount+=1;
-                }
-            }
-        }
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(grid[i][j]==1){
-                    boolean flag=false;
-                    for(int m=i+1;m<row;m++){
-                        if(grid[m][j]==1 || grid[m][j]==-1){
-                            grid[m][j]=-1;
-                            flag=true;
-                        }
-                    }
-                    for(int n=j+1;n<col;n++){
-                        if(grid[i][n]==1 || grid[i][n]==-1){
-                            grid[i][n]=-1;
-                            flag=true;
-                        }
-                    }
-                    for(int k=i-1;k>-1;k--){
-                        if(grid[k][j]==1 || grid[k][j]==-1){
-                            grid[k][j]=-1;
-                            flag=true;
-                        }
-                    }
-                    for(int l=j-1;l>-1;l--){
-                        if(grid[i][l]==1 || grid[i][l]==-1){
-                            grid[i][l]=-1;
-                            flag=true;
-                        }
-                    }
-                    if(!flag){
-                        serverCount--;}
+                if(grid[i][j]==1 && check(grid,i,j,row,col)){
+                    serverCount++;   
                 }
             }
         }
         return serverCount;
+    }
+    public boolean check(int[][] grid,int i,int j,int row,int col){
+        for(int m=i+1;m<row;m++){
+            if(grid[m][j]==1){
+                return true;
+            }
+        }
+        for(int n=j+1;n<col;n++){
+            if(grid[i][n]==1){
+                return true;
+            }
+        }
+        for(int k=i-1;k>-1;k--){
+            if(grid[k][j]==1){
+                return true;
+            }
+        }
+        for(int l=j-1;l>-1;l--){
+            if(grid[i][l]==1){
+                return true;
+            }
+        }
+        return false;
     }
 }
