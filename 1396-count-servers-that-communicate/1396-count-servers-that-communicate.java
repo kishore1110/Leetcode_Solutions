ff6d8a64abf0e909b42e36bcbd70add37e9c173b
Kishore <ganapathinaicker@gmail.com>
2025-01-24 00:40:43 +0530

    Time: 4 ms (23.87%), Space: 52.3 MB (76.74%) - LeetHub

--- 1396-count-servers-that-communicate/1396-count-servers-that-communicate.java
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.java
@@ -15,27 +15,30 @@ class Solution {
                 if(grid[i][j]==1){
                     boolean flag=false;
                     for(int m=i+1;m<row;m++){
-                        if(grid[m][j]==1 && m!=i){
+                        if(grid[m][j]==1 || grid[m][j]==-1){
+                            grid[m][j]=-1;
                             flag=true;
                         }
                     }
                     for(int n=j+1;n<col;n++){
-                        if(grid[i][n]==1 && n!=j){
+                        if(grid[i][n]==1 || grid[i][n]==-1){
+                            grid[i][n]=-1;
                             flag=true;
                         }
                     }
                     for(int k=i-1;k>-1;k--){
-                        if(grid[k][j]==1 && k!=i){
+                        if(grid[k][j]==1 || grid[k][j]==-1){
+                            grid[k][j]=-1;
                             flag=true;
                         }
                     }
                     for(int l=j-1;l>-1;l--){
-                        if(grid[i][l]==1 && l!=j){
+                        if(grid[i][l]==1 || grid[i][l]==-1){
+                            grid[i][l]=-1;
                             flag=true;
                         }
                     }
                     if(!flag){
-                        System.out.println(i+" "+j);
                         serverCount--;}
                 }
             }
