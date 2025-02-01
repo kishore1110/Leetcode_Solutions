bda5030fa6d4636e819645475c731c18f73665cc
Kishore <ganapathinaicker@gmail.com>
2025-02-01 18:23:11 +0530

    Time: 0 ms (100%), Space: 43.1 MB (93.15%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.java
+++ 3429-special-array-i/3429-special-array-i.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isArraySpecial(int[] nums) {
         for(int i=0;i<nums.length-1;i++){
-            if(nums[i]%2==nums[i+1]%2)return false;
+            if((nums[i]&1)==(nums[i+1]&1))return false;
         }
         return true;
     }
