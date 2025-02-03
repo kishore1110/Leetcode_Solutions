a7a014bf954ca711d51f77bba9b046c96f069693
Kishore <ganapathinaicker@gmail.com>
2025-02-03 22:00:02 +0530

    Time: 1 ms (93.94%), Space: 42.5 MB (91.92%) - LeetHub

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.java
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.java
@@ -1,30 +1,29 @@
 class Solution {
     public int longestMonotonicSubarray(int[] nums) {
         int maxCount=0;
-        int currCount=0;
+        int currCount=1;
+        int decCount=1;
         // increaseing order
         for(int i=1;i<nums.length;i++){
             if(nums[i]>nums[i-1]){
+                maxCount=Math.max(decCount,maxCount);
+                decCount=1;
                 currCount++;
             }
-            else{
-                maxCount=Math.max(currCount+1,maxCount);
-                currCount=0;
-            }
-        }
-        maxCount=Math.max(currCount+1,maxCount);
-        // decreasing order
-        currCount=0;
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]<nums[i-1]){
-                currCount++;
+            else if(nums[i]<nums[i-1]){
+                maxCount=Math.max(currCount,maxCount);
+                currCount=1;
+                decCount++;
             }
             else{
-                maxCount=Math.max(currCount+1,maxCount);
-                currCount=0;
+                maxCount=Math.max(currCount,maxCount);
+                maxCount=Math.max(decCount,maxCount);
+                decCount=1;
+                currCount=1;
             }
         }
-        maxCount=Math.max(currCount+1,maxCount);
+        maxCount=Math.max(currCount,maxCount);
+        maxCount=Math.max(decCount,maxCount);
         return maxCount;
     }
 }
