8d7004ba3d39a84e21ea85b7e3857721a617421a
Kishore <ganapathinaicker@gmail.com>
2025-02-22 11:02:37 +0530

    Time: 406 ms (5.1%), Space: 45.1 MB (7.68%) - LeetHub

--- 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.java
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.java
@@ -1,26 +1,41 @@
 class Solution {
     public int punishmentNumber(int n) {
-        int result=0;
-         for(int i=1;i<=n;i++){
-            if(isPunishment(0, 0, i, String.valueOf(i*i))){
-                result+=i*i;
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            String squareStr = String.valueOf(i * i);
+            Map<String, Boolean> memo = new HashMap<>(); // Memoization map
+            if (isPunishment(0, 0, i, squareStr, memo)) {
+                result += i * i;
             }
-         }
-         return result;
+        }
+        return result;
     }
-    public boolean isPunishment(int index,int currSum,int target,String value){
-        if(index>=value.length() && currSum==target){
-            return true;
+
+    public boolean isPunishment(int index, int currSum, int target, String value, Map<String, Boolean> memo) {
+        // Base case: If we have processed the entire string and sum matches the target
+        if (index >= value.length()) {
+            return currSum == target;
         }
-        if(currSum>target){
-            return false;
+
+        // Create a key for memoization
+        String key = index + "," + currSum;
+        if (memo.containsKey(key)) {
+            return memo.get(key);
         }
-        for(int j=index;j<value.length();j++){
-            String currString=value.substring(index,j+1);
-            if(isPunishment(j+1,currSum+Integer.valueOf(currString),target,value)){
+
+        // Try splitting the string at different points
+        for (int j = index; j < value.length(); j++) {
+            String currString = value.substring(index, j + 1);
+            int num = Integer.parseInt(currString);
+
+            if (isPunishment(j + 1, currSum + num, target, value, memo)) {
+                memo.put(key, true);
                 return true;
             }
         }
+
+        // Store result in memoization table
+        memo.put(key, false);
         return false;
     }
 }
