dacac9b1b14b53866ba97aa03680da79f80e286a
Kishore <ganapathinaicker@gmail.com>
2025-02-27 00:26:58 +0530

    Time: 17 ms (34.04%), Space: 44.2 MB (42.21%) - LeetHub

--- 1160-letter-tile-possibilities/1160-letter-tile-possibilities.java
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.java
@@ -1,34 +1,21 @@
 class Solution {
+    int count=-1;
     public int numTilePossibilities(String tiles) {
-        int[] counts = new int[26];
-        int[] fac = new int[tiles.length() + 1];
-        fac[0] = 1;
-        for(int i = 1; i < fac.length; i++){
-            fac[i] = i * fac[i - 1];
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(int i=0;i<tiles.length();i++){
+            map.put(tiles.charAt(i),map.getOrDefault(tiles.charAt(i),0)+1);
         }
-        for(int i = 0; i < tiles.length(); i++){
-            counts[tiles.charAt(i) - 'A']++;
-        }
-        int[] lengthCounts = new int[tiles.length() + 1];
-        lengthCounts[0] = 1;
-        for(int i = 0; i < counts.length; i++){
-            if(counts[i] != 0){
-                int[] temp = new int[tiles.length() + 1];
-                for(int j = 0; j < lengthCounts.length && lengthCounts[j] > 0; j++){
-                    for(int k = 1; k <= counts[i]; k++){
-                        int totalLength = j + k;
-                        temp[totalLength] += lengthCounts[j] * fac[totalLength] / (fac[k] * fac[j]);
-                    }
-                }
-                for(int j = 0; j < temp.length; j++){
-                    lengthCounts[j] += temp[j];
-                }
+        backtrack(map);
+        return count;
+    }
+    public void backtrack(HashMap<Character,Integer> map){
+        count++;
+        for(char key:map.keySet()){
+            if(map.get(key)>0){
+                map.put(key,map.get(key)-1);
+                backtrack(map);
+                map.put(key,map.get(key)+1);
             }
         }
-        int ans = 0;
-        for(int i = 1; i < lengthCounts.length; i++){
-            ans += lengthCounts[i];
-        }
-        return ans;
     }
 }
