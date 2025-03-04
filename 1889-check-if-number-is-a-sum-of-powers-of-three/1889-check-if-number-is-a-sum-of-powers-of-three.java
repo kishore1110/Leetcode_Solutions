b2e8de475429f29576a872c32f7564d4162d24ab
Kishore <ganapathinaicker@gmail.com>
2025-03-04 21:27:35 +0530

    Time: 0 ms (100%), Space: 40.5 MB (43.9%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.java
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.java
@@ -1,22 +1,27 @@
 class Solution {
     public boolean checkPowersOfThree(int n) {
-    //     while(n>0){
-    //         if(n%3==2){
-    //             return false;
-    //         }
-    //         n/=3;
-    //    }
-    //    return true;
-        return backtrack(n,0,0);
+        while(n>0){
+            if(n%3==2){
+                return false;
+            }
+            n/=3;
+       }
+       return true; 
     }
-
-    public boolean backtrack(int n,int power,int currentSum){
-        if(currentSum==n){
-            return true;
-        }
-        if(currentSum>n || Math.pow(3,power)>n){
-            return false;
-        }
-        return backtrack(n,power+1,currentSum+(int)Math.pow(3,power)) || backtrack(n,power+1,currentSum);
-    }
+    // O(log 3 n)
 }
+
+    // public boolean backtrack(int n,int power,int currentSum){
+    //     if(currentSum==n){
+    //         return true;
+    //     }
+    //     if(currentSum>n || Math.pow(3,power)>n){
+    //         return false;
+    //     }
+    //     return backtrack(n,power+1,currentSum+(int)Math.pow(3,power)) || backtrack(n,power+1,currentSum);
+    // }
+
+// time complexity for bactack => 3 power i= n
+// taking log 3 on both sides
+// i=log 3 n
+// tn=> O(2 power log 3 n) which less than n (lot of math behind this)
