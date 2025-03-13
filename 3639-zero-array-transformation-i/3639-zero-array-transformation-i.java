20d9a73efb7a0f2ad50953da2203dde77fc548e2
Kishore <ganapathinaicker@gmail.com>
2025-03-13 19:21:08 +0530

    Time: 3 ms (80.91%), Space: 94.9 MB (67.31%) - LeetHub

---
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isZeroArray(int[] nums, int[][] queries) {
+        int[] differenceArray=new int[nums.length];
+        differenceArray[0]=nums[0];
+        for(int i=1;i<nums.length;i++){
+            differenceArray[i]=nums[i]-nums[i-1];
+        }
+        for(int[] query:queries){
+            int l=query[0];
+            int r=query[1];
+            differenceArray[l]-=1;
+            if(r!=nums.length-1)differenceArray[r+1]+=1;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(i==0){
+                nums[i]=differenceArray[i];
+            }
+            else{
+                nums[i]=nums[i-1]+differenceArray[i];
+            }
+            if(nums[i]>0)return false;
+        }
+        return true;
+    }
+}
