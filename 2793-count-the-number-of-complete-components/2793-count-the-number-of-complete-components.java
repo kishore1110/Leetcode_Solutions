fa428cdfcc9157fc7817cf3ae4f1b1be4e3e376c
Kishore <ganapathinaicker@gmail.com>
2025-03-22 17:56:24 +0530

    Time: 16 ms (28.08%), Space: 45.2 MB (56.68%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.java
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.java
@@ -3,58 +3,41 @@ class Solution {
     int[] size;
     public int countCompleteComponents(int n, int[][] edges) {
         HashMap<Integer,ArrayList<Integer>> adj=new HashMap<>();
-        parent=new int[n];
-        size=new int[n];
-        for(int i=0;i<n;i++){
-            parent[i]=i;
-            size[i]=1;
-        }
         for(int[] edg:edges){
-            unionFind(edg[0],edg[1]);
             adj.computeIfAbsent(edg[0],k->new ArrayList<>()).add(edg[1]);
             adj.computeIfAbsent(edg[1],k->new ArrayList<>()).add(edg[0]);
         }
-        for(int i=0;i<n;i++){
-            parent[i]=findParent(parent[i]);
-        }
-        HashMap<Integer,Integer> map=new HashMap<>();
+        boolean[] visited=new boolean[n];
         int result=0;
-        for(int p:parent){
-            map.put(p,map.getOrDefault(p,0)+1);
-        }
-        for(int key:map.keySet()){
-            if(!adj.containsKey(key)){
-                result++;
-            }
-            else if(map.get(key)-1==adj.get(key).size()){
-                boolean valid=true;
-                for(int node:adj.get(key)){
-                    if(map.get(key)-1!=adj.get(node).size()){
+        for(int i=0;i<n;i++){
+            if(visited[i])continue;
+            ArrayList<Integer> component=dfs(i,adj,visited,new ArrayList<>());
+            boolean valid=true;
+            if(component.size()>1){
+                for(int node:component){
+                    if(adj.get(node).size()!=component.size()-1){
                         valid=false;
+                        break;
                     }
                 }
-                if(valid)result++;
             }
+            if(valid)result++;
         }
         return result;
     }
-    public int findParent(int node){
-        int currentParent=parent[node];
-        while(currentParent!=parent[currentParent]){
-            currentParent=parent[parent[currentParent]];
+    public ArrayList<Integer> dfs(int node,HashMap<Integer,ArrayList<Integer>> adj,boolean[] visited,ArrayList<Integer> component){
+        if(visited[node]){
+            return null;
         }
-        return currentParent;
-    }
-    public void unionFind(int node1,int node2){
-        int root1=findParent(node1);
-        int root2=findParent(node2);
-        if(size[root1]>=size[root2]){
-            parent[root2]=root1;
-            size[root1]+=size[root2];
+        visited[node]=true;
+        component.add(node);
+        if(adj.containsKey(node)){
+            for(int n:adj.get(node)){
+                if(!visited[n]){
+                    dfs(n,adj,visited,component);
+                }
+            }
         }
-        else{
-            parent[root1]=root2;
-            size[root2]+=size[root1];
-        }
+        return component;
     }
 }
