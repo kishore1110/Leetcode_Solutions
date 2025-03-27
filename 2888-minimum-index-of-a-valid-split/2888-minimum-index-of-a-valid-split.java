eee103053cbf5bffd84d4af104e7fe5dc35289e6
Kishore <ganapathinaicker@gmail.com>
2025-03-27 15:36:00 +0530

    Time: 12 ms (80.13%), Space: 56.4 MB (86.54%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.java
@@ -1,22 +1,46 @@
 class Solution {
     public int minimumIndex(List<Integer> nums) {
-        int N=nums.size();
-        HashMap<Integer,Integer> map=new HashMap<>();
-        int dominant=0;
+        // int N=nums.size();
+        // HashMap<Integer,Integer> map=new HashMap<>();
+        // int dominant=0;
+        // for(int num:nums){
+        //     map.put(num,map.getOrDefault(num,0)+1);
+        //     if(dominant==0 && map.get(num)>N/2){
+        //         dominant=num;
+        //     }
+        // }
+        // int totalCount=map.get(dominant);
+        // int Currcount=0;
+        // for(int i=0;i<N;i++){
+        //     if(nums.get(i)==dominant)Currcount++;
+        //     if(Currcount>(i+1)/2 && totalCount-Currcount>(N-i-1)/2){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+        // Without HashMap(what about boyer major algorithm)
+        int candidate=0;
+        int count=0;
         for(int num:nums){
-            map.put(num,map.getOrDefault(num,0)+1);
-            if(map.get(num)>N/2){
-                dominant=num;
+            if(count==0){
+                candidate=num;
             }
+            count=num==candidate?count+1:count-1;
         }
-        int totalCount=map.get(dominant);
+        int totalCount=0;
+        for(int num:nums){
+            if(num==candidate)totalCount++;
+        }
         int Currcount=0;
+        int N=nums.size();
         for(int i=0;i<N;i++){
-            if(nums.get(i)==dominant)Currcount++;
+            if(nums.get(i)==candidate)Currcount++;
             if(Currcount>(i+1)/2 && totalCount-Currcount>(N-i-1)/2){
                 return i;
             }
         }
         return -1;
+
     }
 }
