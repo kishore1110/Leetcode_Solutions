2b339fd119cf49001a23e92cec893b20e5425686
Kishore <ganapathinaicker@gmail.com>
2025-03-30 15:39:35 +0530

    Time: 8 ms (49.76%), Space: 42.2 MB (60.91%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.java
+++ 0768-partition-labels/0768-partition-labels.java
@@ -5,25 +5,18 @@ class Solution {
         HashSet<Integer> set=new HashSet<>();
         int prev=0;
         for(int i=0;i<s.length();i++){
-            freq[s.charAt(i)-'a']++;
+            freq[s.charAt(i)-'a']=i;
         }
         for(int i=0;i<s.length();i++){
             set.add(s.charAt(i)-'a');
-            freq[s.charAt(i)-'a']--;
-            if(check(set,freq)){
+            if(i==freq[s.charAt(i)-'a']){
+                set.remove(s.charAt(i)-'a');
+            }
+            if(set.size()==0){
                 result.add((i+1)-prev);
                 prev=i+1;
-                set.clear();
             }
         }
         return result;
-    }
-    public boolean check(HashSet<Integer> set,int[] freq){
-        for(int i:set){
-            if(freq[i]>0){
-                return false;
-            }
-        }
-        return true;
     }
 }
