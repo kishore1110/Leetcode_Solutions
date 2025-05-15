779de87dbccac9a9b15f017caafd60a18fad2f47
Kishore <ganapathinaicker@gmail.com>
2025-05-15 19:45:31 +0530

    Time: 11 ms (47.15%), Space: 44.9 MB (52.07%) - LeetHub

--- 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.java
+++ 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] findEvenNumbers(int[] digits) {
         Arrays.sort(digits);
         HashSet<Integer> set=new HashSet<>();
-        HashSet<Integer> result=new HashSet<>();
+        ArrayList<Integer> result=new ArrayList<>();
         int index=0;
         while(digits[index]==0){
             index++;
@@ -14,17 +14,16 @@ class Solution {
         for(int num:result){
             resultArray[index++]=num;
         }
-        Arrays.sort(resultArray);
         return resultArray;
     }
-    public void backtrack(int[] digits,HashSet<Integer> set,int index,HashSet<Integer> result,int num,int len){
+    public void backtrack(int[] digits,HashSet<Integer> set,int index,ArrayList<Integer> result,int num,int len){
         if(len>=3){
             if(num%2==0)result.add(num);
             return;
         }
         for(int i=index;i<digits.length;i++){
             if(!set.contains(i)){
-                // if(digits[i]==0 && first==1)continue;
+                if(!set.contains(i-1) && i!=0 && digits[i]==digits[i-1])continue;
                 set.add(i);
                 backtrack(digits,set,0,result,num*10+digits[i],len+1);
                 set.remove(i);
