6a93d34d6f870ff27d5e647e5d95a63776b9dd5d
Kishore <ganapathinaicker@gmail.com>
2025-05-25 23:10:16 +0530

    Time: 54 ms (100%), Space: 46.5 MB (100%) - LeetHub

--- 3857-find-maximum-number-of-non-intersecting-substrings/3857-find-maximum-number-of-non-intersecting-substrings.java
+++ 3857-find-maximum-number-of-non-intersecting-substrings/3857-find-maximum-number-of-non-intersecting-substrings.java
@@ -3,20 +3,19 @@ class Solution {
         int n = word.length();
         Map<Character, Integer> map = new HashMap<>();
         int count = 0;
-
         for (int i = 0; i < n; i++) {
             char ch = word.charAt(i);
             if (map.containsKey(ch)) {
                 int idx = map.get(ch);
                 if (i - idx + 1 >= 4) {
                     count++;
-                    map.clear();
+                    map.clear(); // i came up with this logic but i write map.remove(ch) only 
+                    // think about it
                 }
             } else {
                 map.put(ch, i);
             }
         }
-
         return count;
     }
 }
