d5854afc42f3851fc4f4863aee43ba4babd24785
Kishore <ganapathinaicker@gmail.com>
2025-06-02 14:11:51 +0530

    Time: 27 ms (56.18%), Space: 45.4 MB (52.35%) - LeetHub

---
+++ 0895-shortest-path-to-get-all-keys/0895-shortest-path-to-get-all-keys.java
@@ -0,0 +1,59 @@
+class Solution {
+    int[][] directions={{1,0},{-1,0},{0,1},{0,-1}};
+    public int shortestPathAllKeys(String[] grid) {
+        // bitmask bfs
+        int sX=0;
+        int sY=0;
+        int m=grid.length;
+        int n=grid[0].length();
+        int totalKeys=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i].charAt(j)=='@'){
+                    sX=i;
+                    sY=j;
+                }
+                else if(Character.isLowerCase(grid[i].charAt(j))){
+                    totalKeys++;
+                }
+            }
+        }
+        Queue<int[]> queue=new LinkedList<>();
+        int[][][] visited=new int[m][n][1<<totalKeys];
+        queue.offer(new int[]{sX,sY,0,0});
+        visited[sX][sY][0]=1;
+        while(!queue.isEmpty()){
+            int[] polled=queue.poll();
+            int x=polled[0];
+            int y=polled[1];
+            int keyMask=polled[2];
+            int steps=polled[3];
+            if(check(keyMask,totalKeys)){
+                return steps;
+            }
+            for(int[] dir:directions){
+                int nx=x+dir[0];
+                int ny=y+dir[1];
+                if(nx<0 || ny<0 || nx>=m || ny>=n || grid[nx].charAt(ny)=='#')continue;
+                if(Character.isUpperCase(grid[nx].charAt(ny))){
+                    int pos=grid[nx].charAt(ny)-'A';
+                    if((keyMask&(1<<pos))==0){
+                        continue;
+                    }
+                }
+                int newKey=keyMask;
+                if(Character.isLowerCase(grid[nx].charAt(ny))){
+                    int pos=grid[nx].charAt(ny)-'a';
+                    newKey=keyMask|1<<pos;
+                }
+                if(visited[nx][ny][newKey]==1)continue;
+                visited[nx][ny][newKey]=1;
+                queue.offer(new int[]{nx,ny,newKey,steps+1});
+            }
+        }
+        return -1;
+    }
+    public boolean check(int keyMask,int total){
+        return keyMask == (1 << total) - 1;
+    }
+}
