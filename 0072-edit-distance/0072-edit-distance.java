a86c9a84ab5879b900a13080974f165b1ca41294
Kishore <ganapathinaicker@gmail.com>
2025-06-22 22:02:54 +0530

    Time: 3 ms (93.91%), Space: 45.3 MB (30.78%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -8,21 +8,23 @@ class Solution {
     }
     public int dp(String word1, String word2,int index1,int index2,int[][] memo){
         if(index2>=word2.length()){
-            return word1.length()-index1;
+            return word1.length()-index1;   //want to remove
         }
         if(index1>=word1.length()){
-            return word2.length()-index2;
+            return word2.length()-index2;   //want to insert
         }
         if(memo[index1][index2]!=-1){
             return memo[index1][index2];
         }
         int ans=Integer.MAX_VALUE;
         if(word1.charAt(index1)==word2.charAt(index2)){
-            ans=Math.min(ans,dp(word1,word2,index1+1,index2+1,memo));    //match
+            ans=dp(word1,word2,index1+1,index2+1,memo); //match
         }
-        ans=Math.min(ans,1+dp(word1,word2,index1+1,index2,memo));    //delete
-        ans=Math.min(ans,1+dp(word1,word2,index1+1,index2+1,memo));  //replace
-        ans=Math.min(ans,1+dp(word1,word2,index1,index2+1,memo));   //insert
+        else{
+            ans=Math.min(ans,1+dp(word1,word2,index1+1,index2,memo));    //delete
+            ans=Math.min(ans,1+dp(word1,word2,index1+1,index2+1,memo));  //replace
+            ans=Math.min(ans,1+dp(word1,word2,index1,index2+1,memo));   //insert
+        }
         memo[index1][index2]=ans;    
         return ans;
     }
