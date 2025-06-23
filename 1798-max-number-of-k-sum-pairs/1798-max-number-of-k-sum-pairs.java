a81b47053cb51564d219e856602d4a749a706b4f
Kishore <ganapathinaicker@gmail.com>
2025-06-23 17:37:26 +0530

    Time: 17 ms (99.39%), Space: 57.4 MB (37.4%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.java
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.java
@@ -1,36 +1,36 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
-        // Arrays.sort(nums);
-        // int left=0;
-        // int right=nums.length-1;
-        // int result=0;
-        // while(left<right){
-        //     int sum=nums[left]+nums[right];
-        //     if(sum==k){
-        //         result++;
-        //         left++;
-        //         right--;
-        //     }
-        //     else if(sum<k){
-        //         left++;
-        //     }
-        //     else{
-        //         right--;
-        //     }
-        // }
-        // return result;
-        HashMap<Integer,Integer> map=new HashMap<>();
+        Arrays.sort(nums);
+        int left=0;
+        int right=nums.length-1;
         int result=0;
-        for(int num:nums){
-            int required=k-num;
-            if(map.containsKey(required) && map.get(required)>0){
+        while(left<right){
+            int sum=nums[left]+nums[right];
+            if(sum==k){
                 result++;
-                map.put(required,map.get(required)-1);
+                left++;
+                right--;
+            }
+            else if(sum<k){
+                left++;
             }
             else{
-                map.put(num,map.getOrDefault(num,0)+1);
+                right--;
             }
         }
         return result;
+        // HashMap<Integer,Integer> map=new HashMap<>();
+        // int result=0;
+        // for(int num:nums){
+        //     int required=k-num;
+        //     if(map.containsKey(required) && map.get(required)>0){
+        //         result++;
+        //         map.put(required,map.get(required)-1);
+        //     }
+        //     else{
+        //         map.put(num,map.getOrDefault(num,0)+1);
+        //     }
+        // }
+        // return result;
     }
 }
